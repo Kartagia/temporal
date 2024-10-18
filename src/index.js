d8f73b09298fc91e11a4097bbd0f45e90fd52465
Antti Kautiainen <antti@kautiainen.com>
2024-10-18 11:11:09 +0300

    feat: main library module
    The main library module creating the exported library from
    source modules.
    
    - Changes to be committed:
    -	new file:   src/index.js
      - feat: interface TemporalFieldOptions
      - feat: interface TemporalFieldProperties
      - feat: interface TemporalFieldMethods
      - feat: function createDay

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+/**
+ * Library @Kartagia/temporal implements Temporal library set.
+ */
+
+import {createDay} from "./temporal.mjs";
+
+/**
+ * The temporal field creation options.
+ * @typedef {@import("./temporal.mjs").TemporalOptions} TemporalFieldOptions
+ */
+
+
+/**
+ * The properties shared by all temporal fields. 
+ * @typedef {@import("./temporal.mjs").TemporalFieldProperties} TemporalFieldProperties
+ */
+
+
+/**
+ * The methods shared by all temporal fields. 
+ * @typedef {@import("./temporal.mjs").TemporalFieldMethods} TemporalFieldMethods
+ */
+
+export default {
+    createDay
+}
