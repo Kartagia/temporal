51f1e2ac0156f1754a365bcc33edd7090237c478
Antti Kautiainen <antti@kautiainen.com>
2024-10-24 20:00:26 +0300

    test: createEra and creatYearOfEra works
    The test script proving the createEra and createYearOfEra works.
    - Changes to be committed:
    -	modified:   test/era/createEra.mjs
      - feat: program outputting proof that rudimentary functionality
      without syntax errors for createYearOfEra and createEra exists.

--- test/era/createEra.mjs
+++ test/era/createEra.mjs
@@ -3,7 +3,7 @@
  * 
  */
 
-import {createEra, DateField} from "../../src/temporal.mjs";
+import {createEra, createYearOfEra, DateField} from "../../src/temporal.mjs";
 
 function rangeToString(range) {
     return `Range: [${range.min ?? Number.MIN_SAFE_INTEGER}, ${range.max ?? Number.MAX_SAFE_INTEGER}]`;
@@ -22,4 +22,15 @@ try {
     console.table({canonical: bce.range("canonicalYear"), era: bce.range(DateField.Year), descending: bce.options.descending});
 } catch(error) {
     console.log("Creation failed due error", error);
+}
+
+try {
+    const ce = createEra(1, {name: "Common Era", suffix: "CE", minYear: 1, maxYear: 999999999});
+    const bce = createEra(1, {name: "Before Common Era", suffix: "BCE", minYear: 0, maxYear: -999999999});
+    const bc1 = createYearOfEra(1, {era: bce});
+    console.table(bc1);
+    const ad1 = createYearOfEra(1, {era: ce})
+    console.table(ad1);
+} catch(error) {
+    console.log("Creation of year of era failed", error);
 }
